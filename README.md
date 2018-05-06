6a4c42d6b5afd133b6cbdc4d9afe3e0abba60ba4
Pranay Kumar <Pranay.Kumar@alpha-sense.com>
2018-05-06 19:59:18 +0530

    initial Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# dmux-jarnbjorn-backend Application
+
+Will provide backend services as restful webservice. Can be accessed via web API's consuming and returning json.
+
+## Stack
+### Prerequisites:
+- Java 8
+- Maven
+- Spring boot
+- Spring Security
+- MySql
+
+To enable cache statistics `dev` profile needs to be turned on.
+
+### Authentication
+
+```
+
+```
+
+### Accessing secured endpoints
+
+```
+
+```
+
---
+++ src/main/java/com/dmux/jarnbjorn/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.dmux.jarnbjorn.service;
+
+import com.dmux.jarnbjorn.model.security.User;
+import com.dmux.jarnbjorn.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user != null) {
+            return user;
+        }
+
+        throw new UsernameNotFoundException(username);
+    }
+}
